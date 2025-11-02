0708576f7219a419d50138b0de451e0aff510a67
daliuxz <david.tissera1021@gmail.com>
2025-11-02 20:38:20 -0300

    changes on topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,8 +2,8 @@
 
 export const TopBar = () => {
     return (
-        <header className="bg-slate-900 text-white p-5 text-center">
-            <h1>Mini proyectos de David</h1>
+        <header className="bg-blue-600 text-white p-5 text-center">
+            <h1 className="font-thin">Mini proyectos de <span className="font-bold">David Tissera</span></h1>
         </header>
     );
 }
