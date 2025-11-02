657af237630687db38e101e0f1719941b335eb84
daliuxz <david.tissera1021@gmail.com>
2025-11-02 20:53:22 -0300

    changes color on topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@
 export const TopBar = () => {
     return (
         <header className="bg-blue-600 text-white p-5 text-center">
-            <h1 className="font-thin">Mini proyectos de <span className="font-bold">David Tissera</span></h1>
+            <h1 className="font-thin">Mini proyectos de <span className="font-bold text-red-950">David Tissera</span></h1>
         </header>
     );
 }
